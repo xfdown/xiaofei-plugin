25479dfaf1fcf2cd9fd18cad243a98a8517cde49
小飞 <w22649@139.com>
2022-09-30 12:27:10 +0800

    修改天气指令优先级为100（再不调用我也没办法了。。。）

--- apps/天气.js
+++ apps/天气.js
@@ -14,7 +14,7 @@ export class xiaofei_weather extends plugin {
 			/** https://oicqjs.github.io/oicq/#events */
 			event: 'message',
 			/** 优先级，数字越小等级越高 */
-			priority: 1000,
+			priority: 100,
 			rule: [
 				{
 					/** 命令正则匹配 */
