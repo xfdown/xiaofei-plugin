8e203322fc6fe90c4fdd9dc4972797b56f0caad7
小飞 <w22649@139.com>
2022-10-02 14:37:32 +0800

    v1.5.1:改为图片样式（使用了yunzai_v2版的模板），新增指令：

--- apps/用户ck统计.js
+++ apps/用户ck统计.js
@@ -1,6 +1,10 @@
 import plugin from '../../../lib/plugins/plugin.js'
 import GsCfg from '../../genshin/model/gsCfg.js'
 import lodash from 'lodash'
+import puppeteer from '../../../lib/puppeteer/puppeteer.js'
+import cfg from '../../../lib/config/config.js'
+import { Version } from '.././components/index.js'
+
 /** redis key */
 const keyPre = 'Yz:genshin:mys:';
 const key = {
@@ -16,6 +20,9 @@ const key = {
 	qqUid: `${keyPre}qq-uid:`
 };
 
+const _path = process.cwd();
+const _plugin_path = _path + '/plugins/xiaofei-plugin';
+
 export class xiaofei_userck_statistics extends plugin {
 	constructor () {
 		super({
@@ -30,23 +37,71 @@ export class xiaofei_userck_statistics extends plugin {
 			rule: [
 				{
 					/** 命令正则匹配 */
-					reg: '^#?用户ck统计$',
+					reg: '^#?用户(ck)?统计(uid)?$',
 					/** 执行方法 */
-					fnc: 'query_user_statistics'
+					fnc: 'new_query_user_statistics'
 				}
 			]
 		});
 	}
 	
+	async new_query_user_statistics(){
+		let e = this.e;
+		let {uids, noteCount, botCount} = await this.get_all();
+		let set = GsCfg.getConfig('mys', 'set');
+		let showAll = false;
+		if (!e.isGroup && /uid/.test(e.msg) && e.isMaster) {
+			showAll = true;
+		}
+		let uidCount = 0,
+		disable = 0,
+		available = 0,
+		queryCount = 0,
+		canQuery = 0;
+		lodash.forEach(uids, (uid) => {
+			let count = uid.count;
+			uid.process = Math.min((count / 27) * 100, 100).toFixed(2);
+			uid.type = parseInt(uid.process / 25);
+			canQuery += Math.max(0, 27 - uid.count);
+			if (!showAll) {
+				uid.uid = hideUid(uid.uid);
+			}
+			uidCount++;
+			if (uid.count > 30) {
+				disable++;
+			} else {
+				available++;
+				queryCount += uid.count;
+			}
+		});
+		
+		let data = {
+			plugin_path: _plugin_path,
+			uids,
+			uidCount,
+			queryCount,
+			available,
+			disable,
+			canQuery,
+			noteCount,
+			botCount,
+			useNoteCookie: set.allowUseCookie == 1 ? true : false,
+			showAll,
+			yunzai_ver: `v${cfg.package.version}`,
+			xiaofei_ver: Version.ver
+		};
+		
+		let img = await puppeteer.screenshot("user-stat", {
+			tplFile: `${_plugin_path}/resources/html/user-stat/index.html`,
+			...data
+		});
+		await e.reply(img);
+		return true;
+	}
 	
-		
-	async query_user_statistics(){
-		let msg = [];
-		
+	async get_all(){
 		let res = await GsCfg.getBingCk();
 		let bingCkUid = res.ck
-		let bingCkQQ = res.ckQQ
-		let bingCkLtuid = lodash.keyBy(bingCkUid, 'ltuid')
 		
 		let pubCk = GsCfg.getConfig('mys', 'pubCk') || [];
 		let pub_list = [];
@@ -58,25 +113,124 @@ export class xiaofei_userck_statistics extends plugin {
 			pub_list.push(ltuid);
 		}
 		
-		let bindck_list = bingCkLtuid;
-		//let pub_list = await redis.zRangeByScore(key.count, 0, 999);
-		//let Transfinite_list = await redis.zRangeByScore(key.count,99,99);
+		let bindck_list = bingCkUid;
 		
 		let ck_info = {};
 		let bindck_use_count = 0;
 		let bindck_count = 0;
-		
+
 		for(let uid in bindck_list){
+			let ltuid = bindck_list[uid].ltuid;
+			let count;
+			if(false){
+				count = -1;
+			}else{
+				count = await redis.get(`${key.ckNum}${ltuid}`)
+			}
+			
+			count = Number(count);
+			
+			if(ck_info[ltuid]){
+				ck_info[ltuid] = {
+					count: count,
+					ck_count: ck_info[ltuid].ck_count + 1
+				};
+			}else{
+				ck_info[ltuid] = {
+					count: count,
+					ck_count: 1
+				};
+			}
+			bindck_use_count += count == -1 ? 27 : count;
+			//bindck_count++;
+		}
+		
+		bindck_count = Object.keys(ck_info).length;
+		
+		let pub_count = 0;
+		let pub_use_count = 0;
+		let pub_ck_info = {};
+		
+		for(let ltuid of pub_list){
+			let count;
+			let ck_count = 0;
+			if(ck_info[ltuid]){
+				count = ck_info[ltuid].count;
+				ck_count = ck_info[ltuid].ck_count + 1;
+			}else{
+				if(false){
+					count = -1;
+				}else{
+					count = await redis.get(`${key.ckNum}${ltuid}`)
+				}
+				count = Number(count);
+				ck_count = 1;
+			}
+			pub_ck_info[ltuid] = {
+				count: count,
+				ck_count: ck_count
+			};
+			pub_use_count += count == -1 ? 27 : count;
+		}
+		pub_count = Object.keys(pub_ck_info).length;
+		
+		let uids = [], noteCount = bindck_count, botCount = pub_count;
+		Object.assign(ck_info,pub_ck_info);
+		
+		for(let ltuid in ck_info){
+			let info = ck_info[ltuid];
+			uids.push({
+				uid: ltuid,
+				...info
+			});
+		}
+		noteCount += botCount;
+		return {uids, noteCount, botCount};
+	}
+	
+	async query_user_statistics(){
+		let msg = [];
+		let res = await GsCfg.getBingCk();
+		let bingCkUid = res.ck
+		
+		let pubCk = GsCfg.getConfig('mys', 'pubCk') || [];
+		let pub_list = [];
+		for (let v of pubCk) {
+			let [ltuid = ''] = v.match(/ltuid=(\w{0,9})/g)
+			if (!ltuid) continue
+			ltuid = String(lodash.trim(ltuid, 'ltuid='))
+			if (isNaN(ltuid)) continue
+			pub_list.push(ltuid);
+		}
+		
+		let bindck_list = bingCkUid;
+		
+		let ck_info = {};
+		let bindck_use_count = 0;
+		let bindck_count = 0;
+
+		for(let uid in bindck_list){
+			let ltuid = bindck_list[uid]?.ltuid;
 			let count;
 			if(false){
 				count = -1;
 			}else{
 				count = await redis.get(`${key.ckNum}${uid}`)
 			}
+			
 			count = Number(count);
-			ck_info[uid] = {
-				count: count
-			};
+			
+			if(ck_info[ltuid]){
+				ck_info[ltuid] = {
+					count: count,
+					ck_count: ck_info[ltuid].ck_count + 1
+				};
+			}else{
+				ck_info[ltuid] = {
+					count: count,
+					ck_count: 1
+				};
+			}
 			bindck_use_count += count == -1 ? 27 : count;
 			bindck_count++;
 		}
@@ -135,7 +289,8 @@ export class xiaofei_userck_statistics extends plugin {
 			msg = [];
 			msg.push('---用户ck统计列表---');
 			for(let uid in ck_info){
-				msg.push('['+uid+']使用次数：'+ck_info[uid].count)
+				
+				msg.push('['+uid+']'+(ck_info[uid].ck_count > 1 ? '['+ck_info[uid].ck_count+']' : '')+'使用次数：'+ck_info[uid].count)
 			}
 			msg.push('---公共ck统计列表---');
 			for(let uid in pub_ck_info){
@@ -153,4 +308,13 @@ export class xiaofei_userck_statistics extends plugin {
 		await this.e.reply(forwardMsg);
 		return true;
 	}
+}
+
+function hideUid(uid) {
+  let str = "" + uid,
+    ret = [];
+  for (let idx = 0; idx < 10; idx++) {
+    ret.push(idx > 1 && idx < 5 ? "*" : str[idx]);
+  }
+  return ret.join("");
 }
