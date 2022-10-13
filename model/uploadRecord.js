ff1f5efe66f8222deb5738122bdfa92d5a911ea9
小飞 <w22649@139.com>
2022-10-13 19:26:26 +0800

    【#语音 #高清语音】修复bug

--- model/uploadRecord.js
+++ model/uploadRecord.js
@@ -5,12 +5,12 @@ import querystring from "querystring"
 import fetch from "node-fetch"
 import fs from "fs"
 import path from "path"
-import errors from "oicq"
 import os from "os"
 import util from "util"
 import stream from "stream"
 import crypto from "crypto"
 import child_process from "child_process"
+var errors = {};
 
 
 async function uploadRecord(record_url, seconds = 0,transcoding = true) {
@@ -286,7 +286,7 @@ function parseFunString(buf) {
     if (buf[0] === 0xA) {
         let res = "";
         try {
-            let arr = core_1.pb.decode(buf)[1];
+            let arr = core.pb.decode(buf)[1];
             if (!Array.isArray(arr))
                 arr = [arr];
             for (let v of arr) {
@@ -346,3 +346,74 @@ const NOOP = () => { };
 const pipeline = (0, util.promisify)(stream.pipeline);
 /** md5 hash */
 const md5 = (data) => (0, crypto.createHash)("md5").update(data).digest();
+
+
+errors.LoginErrorCode = errors.drop = errors.ErrorCode = void 0;
+var ErrorCode;
+(function (ErrorCode) {
+    /** 客户端离线 */
+    ErrorCode[ErrorCode["ClientNotOnline"] = -1] = "ClientNotOnline";
+    /** 发包超时未收到服务器回应 */
+    ErrorCode[ErrorCode["PacketTimeout"] = -2] = "PacketTimeout";
+    /** 用户不存在 */
+    ErrorCode[ErrorCode["UserNotExists"] = -10] = "UserNotExists";
+    /** 群不存在(未加入) */
+    ErrorCode[ErrorCode["GroupNotJoined"] = -20] = "GroupNotJoined";
+    /** 群员不存在 */
+    ErrorCode[ErrorCode["MemberNotExists"] = -30] = "MemberNotExists";
+    /** 发消息时传入的参数不正确 */
+    ErrorCode[ErrorCode["MessageBuilderError"] = -60] = "MessageBuilderError";
+    /** 群消息被风控发送失败 */
+    ErrorCode[ErrorCode["RiskMessageError"] = -70] = "RiskMessageError";
+    /** 群消息有敏感词发送失败 */
+    ErrorCode[ErrorCode["SensitiveWordsError"] = -80] = "SensitiveWordsError";
+    /** 上传图片/文件/视频等数据超时 */
+    ErrorCode[ErrorCode["HighwayTimeout"] = -110] = "HighwayTimeout";
+    /** 上传图片/文件/视频等数据遇到网络错误 */
+    ErrorCode[ErrorCode["HighwayNetworkError"] = -120] = "HighwayNetworkError";
+    /** 没有上传通道 */
+    ErrorCode[ErrorCode["NoUploadChannel"] = -130] = "NoUploadChannel";
+    /** 不支持的file类型(没有流) */
+    ErrorCode[ErrorCode["HighwayFileTypeError"] = -140] = "HighwayFileTypeError";
+    /** 文件安全校验未通过不存在 */
+    ErrorCode[ErrorCode["UnsafeFile"] = -150] = "UnsafeFile";
+    /** 离线(私聊)文件不存在 */
+    ErrorCode[ErrorCode["OfflineFileNotExists"] = -160] = "OfflineFileNotExists";
+    /** 群文件不存在(无法转发) */
+    ErrorCode[ErrorCode["GroupFileNotExists"] = -170] = "GroupFileNotExists";
+    /** 获取视频中的图片失败 */
+    ErrorCode[ErrorCode["FFmpegVideoThumbError"] = -210] = "FFmpegVideoThumbError";
+    /** 音频转换失败 */
+    ErrorCode[ErrorCode["FFmpegPttTransError"] = -220] = "FFmpegPttTransError";
+})(ErrorCode = errors.ErrorCode || (errors.ErrorCode = {}));
+const ErrorMessage = {
+    [ErrorCode.UserNotExists]: "查无此人",
+    [ErrorCode.GroupNotJoined]: "未加入的群",
+    [ErrorCode.MemberNotExists]: "幽灵群员",
+    [ErrorCode.RiskMessageError]: "群消息发送失败，可能被风控",
+    [ErrorCode.SensitiveWordsError]: "群消息发送失败，请检查消息内容",
+    10: "消息过长",
+    34: "消息过长",
+    120: "在该群被禁言",
+    121: "AT全体剩余次数不足"
+};
+function drop(code, message) {
+    if (!message || !message.length)
+        message = ErrorMessage[code];
+    throw new core.ApiRejection(code, message);
+}
+errors.drop = drop;
+/** 登录时可能出现的错误，不在列的都属于未知错误，暂时无法解决 */
+var LoginErrorCode;
+(function (LoginErrorCode) {
+    /** 密码错误 */
+    LoginErrorCode[LoginErrorCode["WrongPassword"] = 1] = "WrongPassword";
+    /** 账号被冻结 */
+    LoginErrorCode[LoginErrorCode["AccountFrozen"] = 40] = "AccountFrozen";
+    /** 发短信太频繁 */
+    LoginErrorCode[LoginErrorCode["TooManySms"] = 162] = "TooManySms";
+    /** 短信验证码错误 */
+    LoginErrorCode[LoginErrorCode["WrongSmsCode"] = 163] = "WrongSmsCode";
+    /** 滑块ticket错误 */
+    LoginErrorCode[LoginErrorCode["WrongTicket"] = 237] = "WrongTicket";
+})(LoginErrorCode = errors.LoginErrorCode || (errors.LoginErrorCode = {}));
