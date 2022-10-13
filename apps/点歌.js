b2f998af1a89e366976d02ea243488c29228d912
小飞 <w22649@139.com>
2022-10-13 18:45:42 +0800

    update 点歌.js

--- apps/点歌.js
+++ apps/点歌.js
@@ -428,6 +428,7 @@ async function music_handle(e, search, source, page = 0, page_size = 10, temp_da
 					user_id: e.user_id
 				};
 
+				let MsgList = [];
 				let index = 1;
 				for(let music of result.data){
 					let body = await CreateMusicShare(e,music);
