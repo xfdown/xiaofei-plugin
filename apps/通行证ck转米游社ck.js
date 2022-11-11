36df3c0f74b714924495544edfa2aeb00818e9ff
小飞 <w22649@139.com>
2022-11-11 22:46:37 +0800

    【通行证ck转米游社ck】临时修改判断，防止影响后续绑定。

--- apps/通行证ck转米游社ck.js
+++ apps/通行证ck转米游社ck.js
@@ -93,7 +93,7 @@ export class xiaofei_mysck extends plugin {
 	}
 	
 	async mysck(){
-		if (this.e.msg.includes('login_ticket=') && !this.e.msg.includes('cookie_token=')) {
+		if (this.e.msg.includes('login_ticket=') && !this.e.msg.includes('cookie_token=') && !this.e.msg.includes('cookie_token_v2=')) {
 			if (this.e.isGroup) {
 				this.e.reply('请私聊发送cookie', false, { at: true });
 				this.e.msg = '';
