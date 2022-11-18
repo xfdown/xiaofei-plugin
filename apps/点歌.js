b5d801dad4b8cbba1b21dc22b0f4242938a165f2
小飞 <w22649@139.com>
2022-11-18 18:07:11 +0800

    update

--- apps/点歌.js
+++ apps/点歌.js
@@ -293,7 +293,7 @@ export class xiaofei_music extends plugin {
 		msgs.push(`因网易云音乐ck使用了HttpOnly，手机端需使用抓包工具获取（或使用Via浏览器的"查看Cookies"功能获取），pc端请使用浏览器的开发人员工具获取。`);
 		msgs.push(`--------------------`);
 		msgs.push(`Via浏览器下载：https://www.viayoo.com/`);
-		
+
 		MsgList.push({
 			...user_info,
 			message: `---提交音乐ck说明---\n${msgs.join('\n')}`
@@ -662,7 +662,7 @@ async function music_handle(e, search, source, page = 0, page_size = 10, temp_da
 			page = result.page;
 			let title = source[1] + '点歌列表';
 			if (result.title) title = result.title;
-			title += `[第${page}页]`;
+			if (result.data.length >= page_size) title += `[第${page}页]`;
 			let msg_result = [];
 
 			let setting = Config.getdefSet('setting', 'system') || {};
