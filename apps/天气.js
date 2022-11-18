6c4284a0fa6968647ae1b1529d906104d5b604db
小飞 <w22649@139.com>
2022-11-18 17:49:32 +0800

    fix 天气设置

--- apps/天气.js
+++ apps/天气.js
@@ -32,6 +32,7 @@ export class xiaofei_weather extends plugin {
 	}
 
 	async query_weather() {
+		if (/^#?小飞设置.*$/.test(this.e.msg)) return false;
 		let msg = this.e.msg
 			.replace('#', '')
 			.replace('小飞', '')
