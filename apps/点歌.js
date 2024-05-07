43434b2a5334b47b2f01d7e9a1b47b5e74bfb5ff
caimingtr <1564571378@qq.com>
2024-05-07 14:38:11 +0800

    getMsg

--- apps/点歌.js
+++ apps/点歌.js
@@ -460,7 +460,7 @@ async function music_message(e) {
 						result = await e.reply(result);
 						if (reg[1].includes('高清') && result && isHigh) {
 							try {
-								let message = await Bot[e.self_id].getMsg(result.message_id);
+								let message = await (Bot?.getMsg || e.group?.getMsg || e.friend?.getMsg)?.(result.message_id);
 								if (Array.isArray(message.message)) message.message.push({ type: 'text', text: '[语音]' });
 								(e.group || e.friend)?.sendMsg('PCQQ不要播放，否则会导致语音无声音！', message);
 							} catch (err) {
@@ -517,7 +517,7 @@ async function music_message(e) {
 					result = await e.reply(result)
 					if (reg[1].includes('高清') && result && isHigh) {
 						try {
-							let message = await Bot[e.self_id].getMsg(result.message_id);
+							let message = await (Bot?.getMsg || e.group?.getMsg || e.friend?.getMsg)?.(result.message_id);
 							if (Array.isArray(message.message)) message.message.push({ type: 'text', text: '[语音]' });
 							(e.group || e.friend)?.sendMsg('PCQQ不要播放，否则会导致语音无声音！', message);
 						} catch (err) { }
